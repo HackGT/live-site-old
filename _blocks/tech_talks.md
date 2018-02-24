3424c05d6c9ebb8ddd9596bd801f8f809164a6aa
Evan Strat <evan10s@users.noreply.github.com>
2018-02-24 00:57:26 -0500

    Finished up JS and CSS for schedule generated from Google Calendar
    The "it works" commit

---
+++ _blocks/tech_talks.md
@@ -0,0 +1,7 @@
+---
+gcal_key: xxx
+gcal_name: hackgt_events
+gcal_start_timestamp: blah
+title: Tech Talks
+layout: one_column_schedule
+---
--- _includes/schedule.js
+++ _includes/schedule.js
@@ -19,35 +19,62 @@ function prettyTime(dateObj) {
 function success(result) {
     result.items.sort(function(a,b) {
         return new Date(a.start.dateTime) - new Date(b.start.dateTime)
-            || new Date(a.end.dateTime) - new Date(b.end.dateTime);
+            || new Date(a.end.dateTime) - new Date(b.end.dateTime)
+            || a.summary > b.summary;
         //return 1;
     });
 
+
+    var DAYS_OF_WEEK = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
     var events = result.items;
-    var schedule = $("#schedule-1 > ul");
+    var schedule = $("#schedule-1 > table > tbody");
     var startTime;
     var endTime;
+    var location;
+
+    var prevCurrentDay = new Date(events[0].start.dateTime).getDay();
+    schedule.append('<tr><td class="schedule-day" colspan="4">'
+        + DAYS_OF_WEEK[prevCurrentDay] + '</td></tr>');
+
+    var currentDay;
+
     for (var i = 0; i < events.length; i++) {
-        startTime = prettyTime(new Date(events[i].start.dateTime))
-        endTime = prettyTime(new Date(events[i].end.dateTime))
-        schedule.append("<li>" + events[i].summary + " (" + startTime + "-" + endTime + ") at " + events[i].location +"</li>");
+        if (i > 0) {
+            currentDay = new Date(events[i].start.dateTime).getDay();
+
+            if (currentDay != prevCurrentDay) {
+                schedule.append('<tr><td class="schedule-day" colspan="4">'
+                    + DAYS_OF_WEEK[currentDay] + '</td></tr>');
+            }
+            prevCurrentDay = currentDay;
+        }
+
+        startTime = prettyTime(new Date(events[i].start.dateTime)) || "";
+        endTime = prettyTime(new Date(events[i].end.dateTime)) || "";
+        location = events[i].location || "";
+        schedule.append("<tr><td>" + events[i].summary + "</td><td>" + startTime + "</td><td>" + endTime + "</td><td> " + location + "</td></tr>");
 
     }
 
-    schedule.parent().removeClass("hidden");
+    $('#schedule-1').prev().addClass("hidden");
+    $('#schedule-1').removeClass("hidden");
 }
 
 (function() {
-    //console.log("hi");
-    //x();
-
-    var gcalRequest = new XMLHttpRequest();
-
     var url = "https://www.googleapis.com/calendar/v3/calendars/{{ page.gcal.id }}/events?key={{page.gcal.api_key}}&timeMin={{page.gcal.start_datetime}}&timeMax={{page.gcal.end_datetime}}";
 
     //TODO: error handling
     //TODO: no events exist
-    //TODO: max time later than min time (perhaps show "This page is configured wrong" on the schedule page" or figure out how to make the jekyll build fail)
+    //TODO: max time later than min time (perhaps console.warn? "This page is configured wrong" on the schedule page: ..." or figure out how to make the jekyll build fail)
     $.get(url, success);
 
 })();
