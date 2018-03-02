0ac5c8f11ebfa39c617e94391bc6866d95e051cb
Evan Strat <evan10s@users.noreply.github.com>
2018-03-02 00:16:43 -0500

    General clean up
    Added return in first if statement of success function so that the rest
    of the function doesn't have to be in an else block
    
    Moved variable declarations outside for loops to inside the for loops
    
    Removed unnecessary comments

--- _includes/schedule.js
+++ _includes/schedule.js
@@ -24,94 +24,86 @@
 
         if (result.items.length == 0) {
             $(scheduleId).prev().text("No events found");
-        } else {
-            result.items.sort(function(a,b) {
-                return new Date(a.start.dateTime) - new Date(b.start.dateTime)
-                    || new Date(a.end.dateTime) - new Date(b.end.dateTime)
-                    || a.summary > b.summary;
-                //return 1;
-            });
+            return false;
+        }
+        result.items.sort(function(a,b) {
+            return new Date(a.start.dateTime) - new Date(b.start.dateTime)
+                || new Date(a.end.dateTime) - new Date(b.end.dateTime)
+                || a.summary > b.summary;
+            //return 1;
+        });
 
+        var DAYS_OF_WEEK = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        ];
 
-            var DAYS_OF_WEEK = [
-                "Sunday",
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday"
-            ];
+        var events = result.items;
+        var schedule = $(scheduleId + " > table > tbody");
+        var currentTime = new Date();
 
+        var prevCurrentDay = new Date(events[0].start.dateTime).getDay();
+        schedule.append('<tr><td class="schedule-day" colspan="4">'
+            + DAYS_OF_WEEK[prevCurrentDay] + '</td></tr>');
 
-            var events = result.items;
-            var schedule = $(scheduleId + " > table > tbody");
-            var startTime;
-            var endTime;
-            var endTimeAsDate;
-            var location;
-            var currentTime = new Date();
-            var oldClass;
+        var eventValues;
+        for (var i = 0; i < events.length; i++) {
+            if (i > 0) {
+                var currentDay = new Date(events[i].start.dateTime).getDay();
 
-            var prevCurrentDay = new Date(events[0].start.dateTime).getDay();
-            schedule.append('<tr><td class="schedule-day" colspan="4">'
-                + DAYS_OF_WEEK[prevCurrentDay] + '</td></tr>');
-
-            var currentDay;
-            var eventValues;
-            for (var i = 0; i < events.length; i++) {
-                if (i > 0) {
-                    currentDay = new Date(events[i].start.dateTime).getDay();
-
-                    if (currentDay != prevCurrentDay) {
-                        schedule.append('<tr><td class="schedule-day" colspan="4">'
-                            + DAYS_OF_WEEK[currentDay] + '</td></tr>');
-                    }
-                    prevCurrentDay = currentDay;
+                if (currentDay != prevCurrentDay) {
+                    schedule.append('<tr><td class="schedule-day" colspan="4">'
+                        + DAYS_OF_WEEK[currentDay] + '</td></tr>');
                 }
-
-                startTime = prettyTime(new Date(events[i].start.dateTime));
-                endTimeAsDate = new Date(events[i].end.dateTime)
-                endTime = prettyTime(endTimeAsDate);
-                location = events[i].location || "";
-                oldClass = (currentTime - endTimeAsDate > 0) ? ' class="old"' : ""; //event already ended
-                schedule.append("<tr" + oldClass + "><td></td><td></td><td></td><td></td></tr>");
-                // schedule.append("<tr" + oldClass + "><td>" + events[i].summary + "</td><td>" + startTime + "</td><td>" + endTime + "</td><td> " + location + "</td></tr>");
-                eventValues = [
-                    events[i].summary,
-                    startTime,
-                    endTime,
-                    location
-                ];
-
-                $(scheduleId + ' > table > tbody > tr:last-child > td').each(function(index) {
-                    $(this).text(eventValues[index]);
-                });
+                prevCurrentDay = currentDay;
             }
 
-            $(scheduleId).prev().addClass("hidden");
-            $(scheduleId).removeClass("hidden");
-        } //END else
+            var startTime = prettyTime(new Date(events[i].start.dateTime));
+            var endTimeAsDate = new Date(events[i].end.dateTime)
+            var endTime = prettyTime(endTimeAsDate);
+            var location = events[i].location || "";
+            var oldClass = (currentTime - endTimeAsDate > 0) ? ' class="old"' : ""; //event already ended
+            schedule.append("<tr" + oldClass + "><td></td><td></td><td></td><td></td></tr>");
+
+            eventValues = [
+                events[i].summary,
+                startTime,
+                endTime,
+                location
+            ];
+
+            $(scheduleId + ' > table > tbody > tr:last-child > td').each(function(index) {
+                $(this).text(eventValues[index]);
+            });
+        }
+
+        $(scheduleId).prev().addClass("hidden");
+        $(scheduleId).removeClass("hidden");
     }
 
     function scheduleError(error) {
         var scheduleId = "#schedule-" + this.num;
-        $(scheduleId).prev().text("The schedule couldn't be retrieved.  Please check your internet connection."     );
+        $(scheduleId).prev().text("The schedule couldn't be retrieved.  Please check your internet connection.");
     }
 
     // Show a warning in the console if endDateTime comes before startDateTime
     if (new Date(endDateTime) - new Date(startDateTime) < 0) {
-        console.warn("schedule.js: No calendar events will be fetched!  The date of the last calendar event to include in the schedule (endDateTime) is earlier than the date of the first calendar event to include in the schedule.  The current start date is " + new Date(startDateTime) + ".  The current end date is " + new Date(endDateTime) + ".");
+        console.warn("schedule.js: No calendar events will be fetched!  The date of the last calendar event to include in the schedule (endDateTime) is earlier than the date of the first calendar event to include in the schedule.  The current start date is "
+        + new Date(startDateTime)
+        + ".  The current end date is " + new Date(endDateTime) + ".");
     }
 
-    var url;
-    var calId;
-
     for (var i = 0; i < calendars.length; i++) {
-        calId = calendars[i];
+        var calId = calendars[i];
 
-        url = "https://www.googleapis.com/calendar/v3/calendars/" + calId
-            + "/events?key={{page.gcal.api_key}}&timeMin=" + startDateTime + "&timeMax=" + endDateTime;
+        var url = "https://www.googleapis.com/calendar/v3/calendars/" + calId
+            + "/events?key={{page.gcal.api_key}}&timeMin=" + startDateTime
+            + "&timeMax=" + endDateTime;
 
         $.ajax({
             "url": url,
