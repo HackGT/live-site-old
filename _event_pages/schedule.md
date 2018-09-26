7dd28ab9552b837ecf7a25344f04c12bb6097253
Evan Strat <evan10s@users.noreply.github.com>
2018-09-26 12:25:09 -0400

    Schedule improvements (#63)
    * WIP collapsible days on schedule page
    
    * Collapsible days MVP
    
    * Animation caret rotation on expand/collapse
    
    * Lots of schedule error state improvements
    
    Removed the unused (I hope) jekyll-feed dep
    
    * Remove the Error thrown for testing purposes
    
    * Finished schedule error states
    
    * Add schedule assets
    
    * Don't write to the console
    
    * Replaced a console.warn with an actual error state
    
    Specifically, when the schedule event range end date is before the end date (defined in the .md file for the schedule page),
    the schedule will show an error with this as the reason, rather than just showing "No events found"
    
    * Fixed horizontal overflow issue

--- _event_pages/schedule.md
+++ _event_pages/schedule.md
@@ -2,15 +2,12 @@
 calendars:
     -
       title: Event
-      id: lettersandnumbers@group.calendar.google.com
-    -
-      title: Tech Talks and Workshops
-      id: lettersandnumbers@group.calendar.google.com
+      id: n6hdruu3dtagl8mainglkj0kh0@group.calendar.google.com
 gcal:
   # All calendars on the same page must use the same API key and have the same start/end dates/times
-  api_key: google_api_key
+  api_key: AIzaSyCy-H3QRODFKvDBphR-OgkeNJP6MRxf_V8
   start_datetime: "2018-02-22T18:00:00-0500" #Be sure to enclose this in quotes so Jekyll doesn't interpret as a Date!
-  end_datetime: "2018-02-24T15:00:01-0500" #This is the absolute latest start time to include an event on the page.  The timestamp is exclusive, meaning that to include events at 3:00 PM, you would write T15:01:00 (includes events starting as late as 15:00:59)
+  end_datetime: "2019-02-24T15:00:01-0500" #This is the absolute latest start time to include an event on the page.  The timestamp is exclusive, meaning that to include events at 3:00 PM, you would write T15:01:00 (includes events starting as late as 15:00:59)
 title: Schedule
 layout: schedule
 priority: 2
--- _includes/schedule.js
+++ _includes/schedule.js
@@ -2,6 +2,16 @@
 // This is an anonymous function to keep the functions and variables here out
 // of the global scope
 (function() {
+    function toggleDay(day) {
+        document.querySelectorAll('.schedule-day-' + day).forEach(function (item, index) {
+            item.classList.toggle('hidden');
+        });
+
+        var caret = document.querySelector('tr#header-day-' + day + ' > td > i');
+        caret.classList.toggle('closed');
+        caret.classList.toggle('open');
+    }
+
     function prettyTime(dateObj) {
         var hours = dateObj.getHours();
         var minutes = dateObj.getMinutes();
@@ -33,10 +43,16 @@
     function success(data) {
         var result = data.apiResponse;
         var scheduleId = "#schedule-" + data.num;
+        var scheduleClass = ".schedule-" + data.num;
 
         if (result.items.length == 0) {
-            document.querySelector(scheduleId)
-                .previousElementSibling.textContent = "No events found";
+            // Show a warning in the console if endDateTime comes before startDateTime
+            if (new Date(endDateTime) - new Date(startDateTime) < 0) {
+                scheduleError(data.num, "Event range end date is before start date")
+            } else {
+                document.querySelector(scheduleId)
+                    .previousElementSibling.textContent = "No events found";
+            }
             return false;
         }
         result.items.sort(function(a,b) {
@@ -64,18 +80,28 @@
         if (showDayHeaders) {
 
             var prevCurrentDay = new Date(events[0].start.dateTime).getDay();
-            schedule.insertAdjacentHTML('beforeend','<tr><td class="schedule-day" colspan="4">'+ DAYS_OF_WEEK[prevCurrentDay] + '</td></tr>');
+            schedule.insertAdjacentHTML('beforeend','<tr id="header-day-0" data-day="0"><td class="schedule-day" colspan="4"><i class="material-icons open">keyboard_arrow_down</i>'+ DAYS_OF_WEEK[prevCurrentDay] + '</td></tr>');
+            document.querySelector('#header-day-0').onclick = function() {
+                toggleDay(this.attributes['data-day'].value);
+            };
         }
+
+        var day = 0;
 
         var eventValues;
         for (var i = 0; i < events.length; i++) {
             if (i > 0) {
                 if (showDayHeaders) {
+
                     var currentDay = new Date(events[i].start.dateTime).getDay();
 
                     if (currentDay != prevCurrentDay) {
-                        schedule.insertAdjacentHTML('beforeend','<tr><td class="schedule-day" colspan="4">'
+                        day++;
+                        schedule.insertAdjacentHTML('beforeend','<tr id="header-day-' + day + '" data-day="' + day + '"><td class="schedule-day" colspan="4"><i class="material-icons open" draggable="false">keyboard_arrow_down</i>'
                             + DAYS_OF_WEEK[currentDay] + '</td></tr>');
+                        document.querySelector('#header-day-' + day).addEventListener('click', function() {
+                            toggleDay(this.attributes['data-day'].value);
+                        });
                     }
                     prevCurrentDay = currentDay;
                 }
@@ -92,8 +118,8 @@
                 endTime = "";
             }
             var location = events[i].location || "";
-            var oldClass = (currentTime - endTimeAsDate > 0) ? ' class="old"' : ""; //event already ended
-            schedule.insertAdjacentHTML('beforeend',"<tr" + oldClass + "><td></td><td></td><td></td><td></td></tr>");
+            var oldClass = (currentTime - endTimeAsDate > 0) ? ' old' : ""; //event already ended
+            schedule.insertAdjacentHTML('beforeend','<tr class="schedule-day-'+ day + oldClass + '"><td></td><td></td><td></td><td></td></tr>');
 
             eventValues = [
                 events[i].summary,
@@ -107,23 +133,35 @@
         }
 
         var scheduleBlockBody = document.querySelector(scheduleId);
-        var prevScheduleBlockBody = scheduleBlockBody.previousElementSibling;
-
-        prevScheduleBlockBody.classList.add("hidden");
+        document.querySelector(scheduleClass + ".schedule-loading").classList.add("hidden");
         scheduleBlockBody.classList.remove("hidden");
     }
 
-    function scheduleError(i) {
-        var scheduleId = "#schedule-" + i;
-        var errorElem = document.querySelector(scheduleId);
-        errorElem.previousElementSibling.textContent = "The schedule couldn't be retrieved.  Please check your internet connection."
-    }
+    function scheduleError(i, msg) {
+        var scheduleClass = ".schedule-" + i;
+        var errorDiv = document.querySelector(scheduleClass + ".schedule-error")
+        var title, body;
+        if (msg === "Failed to fetch") {
+            title = "No internet connection";
+            body = "We can't load the schedule right now because your device isn't connected to the internet.  Please check " +
+                "your internet connection and try again."
+        } else {
+            title = "Can't show schedule";
+            body = "Something is preventing us from displaying the schedule.  Contact a member of the HackGTeam for help.";
+        }
 
-    // Show a warning in the console if endDateTime comes before startDateTime
-    if (new Date(endDateTime) - new Date(startDateTime) < 0) {
-        console.warn("schedule.js: No calendar events will be fetched!  The date of the last calendar event to include in the schedule (endDateTime) is earlier than the date of the first calendar event to include in the schedule.  The current start date is "
-        + new Date(startDateTime)
-        + ".  The current end date is " + new Date(endDateTime) + ".");
+        document.querySelector(scheduleClass + ".schedule-error > h4").textContent = title;
+        document.querySelector(scheduleClass + ".schedule-error > p.error-text").textContent = body;
+        document.querySelector(scheduleClass + ".schedule-error > p.error-msg > small").textContent = "Error: " + msg;
+
+        errorDiv.classList.remove("hidden");
+
+        document.querySelector(scheduleClass + ".schedule-loading").classList.add("hidden");
+
+
+
+        //var errorElem = document.querySelector(scheduleClass);
+        //errorElem.previousElementSibling.lastChild.textContent = "The schedule couldn't be retrieved.  Please check your internet connection.  Error: " + msg;
     }
 
     function getCalendarData(url, i) {
@@ -134,8 +172,7 @@
                     num: i });
         })
         .catch(function (e) {
-            console.error(e);
-            scheduleError(i);
+            scheduleError(i, e.message);
         });
     }
 
